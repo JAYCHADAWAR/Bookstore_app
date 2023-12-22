829dc80fae8c2c6d8052200eb3de2b4c80ccd0ab
JAY CHADAWAR <83951655+JAYCHADAWAR@users.noreply.github.com>
2023-12-22 19:37:03 +0530

    updated app.js

--- frontend/book_frontend/src/App.js
+++ frontend/book_frontend/src/App.js
@@ -7,15 +7,40 @@ import Profile from './components/Profile';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
 import Navbar from './components/Navbar';
+import Admin from './components/Admin';
+import Admin_login from './components/Admin_login'
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  
   useEffect(() => {
     const storedLoginStatus = localStorage.getItem('isLoggedIn');
     if (storedLoginStatus === 'true') {
       setIsLoggedIn(true);
     }
+
+    const storedAdminStatus = localStorage.getItem('isAdmin');
+    if (storedAdminStatus === 'true') {
+      setIsAdmin(true);
+    }
   }, []);
+
+
+
+  const handleAdminSignOut = async () => {
+    setIsAdmin(false);
+    localStorage.removeItem('isAdmin');
+    alert('Admin logged out');
+  };
+
+  const handleAdminLogin = async () => {
+    setIsAdmin(true);
+    localStorage.setItem('isAdmin', 'true');
+  };
+
   const handleSignUp = async (e) => {
     setIsLoggedIn(true);
     localStorage.setItem('isLoggedIn', 'true'); 
@@ -47,9 +72,11 @@ function App() {
   return (
     <div>
     <BrowserRouter>
-       <Navbar isLoggedIn={isLoggedIn} handleSignOut={handleSignOut} />
-  
+       <Navbar isLoggedIn={isLoggedIn} isAdmin={isAdmin} handleAdminSignOut={handleAdminSignOut} handleSignOut={handleSignOut} />
+       
     <Routes>
+      <Route path='/Admin' element={isAdmin ? <Admin/> : <h1>admin is not logged in</h1>}/>
+      <Route path='/Admin_signin'  element ={!isAdmin ? <Admin_login handleAdminLogin={handleAdminLogin}/> : null}/>
         <Route exact path="/home" element={isLoggedIn ? <Home /> : <h1>You are not loggedin</h1>}/>
         <Route path="/profile" element={isLoggedIn ? <Profile /> : <h1>You are not loggedin</h1>}  />
         <Route path="/signup"  element={!isLoggedIn ? <SignUp handleSignUp={handleSignUp}/> : null} />
